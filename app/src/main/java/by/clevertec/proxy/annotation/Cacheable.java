48b0cc41eb1a6373e2985edb1da25352d376e4d1
Viktar Rusakovich <mrisviz.rus@gmail.com>
2023-11-30 20:37:17 +0300

    add pdf_creator

--- app/src/main/java/by/clevertec/proxy/annotation/Cacheable.java
+++ app/src/main/java/by/clevertec/proxy/annotation/Cacheable.java
@@ -4,6 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 /**
  * Аннотация {@code Cacheable} используется для обозначения методов, которые должны быть кэшированы.
  * <p>
---
+++ app/src/main/java/by/clevertec/service/impl/UserDocumentService.java
@@ -0,0 +1,27 @@
+package by.clevertec.service.impl;
+
+import by.clevertec.dto.UserDto;
+import by.clevertec.service.DocumentService;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfContentByte;
+import java.util.List;
+
+public class UserDocumentService implements DocumentService {
+    private final List<UserDto> users;
+
+    public UserDocumentService(List<UserDto> users) {
+        this.users = users;
+    }
+
+    @Override
+    public void processDocument(PdfContentByte canvas) {
+        int yPosition = 500;
+        for (UserDto user : users) {
+            String text = "ID: " + user.getId() + ", Имя: " + user.getName() + ", Email: " + user.getEmail() + ", Телефон: " + user.getPhoneNumber();
+            ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, new Phrase(text), 50, yPosition, 0);
+            yPosition -= 50;
+        }
+    }
+}
